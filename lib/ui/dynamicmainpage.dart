9bd2706220a1f5554bd4d7b06ec8c937a9d83b51
unknxwnwxrld <smerechinskiy.oleksii@gmail.com>
2025-06-08 00:47:05 +0200

    Added BottomNavigationBar functionality

---
+++ lib/ui/dynamicmainpage.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:liquidlibrary/widgets/bookcard.dart';
+
+class DynamicMainPage extends StatefulWidget {
+  const DynamicMainPage({super.key});
+
+  @override
+  State<DynamicMainPage> createState() => _DynamicMainPageState();
+}
+
+class _DynamicMainPageState extends State<DynamicMainPage> {
+  int _selectedIndex = 0;
+
+  final List<List<Widget>> _actions = [
+    [
+      IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+      IconButton(onPressed: () {}, icon: Icon(Icons.filter_list)),
+      IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+    ],
+    [],
+    []
+  ];
+
+  final List<Widget> _pages = [
+    Center(child: Text('Library')),
+    Center(child: Text('Profile')),
+    Center(child: Text('Settings')),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 5,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Icon(Icons.water_drop),
+            actions: _actions[_selectedIndex],
+            bottom: _selectedIndex == 0
+                ? const TabBar(
+                    tabs: [
+                      Tab(icon: Icon(Icons.radio_button_unchecked)),
+                      Tab(icon: Icon(Icons.control_point)),
+                      Tab(icon: Icon(Icons.check_circle_outline)),
+                      Tab(icon: Icon(Icons.schedule)),
+                      Tab(icon: Icon(Icons.highlight_off)),
+                    ],
+                  )
+                : null,
+          ),
+          body: _selectedIndex == 0
+              ? TabBarView(
+                  children: [
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: BookCard(
+                        title: 'Повелитель тайн',
+                        author: 'unknown',
+                        coverUrl:
+                            'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1723688384i/58826678.jpg',
+                      ),
+                    ),
+                    Center(child: Text('Planned')),
+                    Center(child: Text('Completed')),
+                    Center(child: Text('Holded')),
+                    Center(child: Text('Dropped')),
+                  ],
+                )
+              : _pages[_selectedIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            showUnselectedLabels: true,
+            type: BottomNavigationBarType.fixed,
+            selectedFontSize: 12,
+            unselectedFontSize: 12,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.book),
+                label: 'Library',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: 'Settings',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/bookcard.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class BookCard extends StatelessWidget {
+  final String title;
+  final String author;
+  final String coverUrl;
+
+  BookCard({required this.title, required this.author, required this.coverUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0), // Margin around the card
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(12.0),
+          onTap: () {},
+          child: Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min, // Fit to content vertically
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.network(
+                    coverUrl,
+                    width: 100.0,
+                    // Remove height to allow vertical fit
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                SizedBox(width: 12.0),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min, // Fit to content vertically
+                    children: <Widget>[
+                      Text(
+                        title,
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      SizedBox(
+                        height: 4.0
+                      ),
+                      Text(
+                        author,
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.grey[600]
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
